3141c20a2c5c4197240d20e6c68d8d6697b11784
JhnMrcco <marco.resuello@gmail.com>
2016-11-28 15:36:48 +0800

    Add files via upload
    I have created a GUI, Game Modes(Bordered and Borderless), the user can change Body and Head color of the snake, as well as the color of the Food. Monster is not included in this patch. Hope this can help you :)

---
+++ JhnMrcco-master/Snake3.0/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.BackgroundImage = (My.Resources.Main)
+    End Sub
+
+    Private Sub cmdStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdStart.Click
+
+        Me.Hide()
+        FrmMAin.Show()
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        'Determines what keys are pressed and what to do when each one is pressed
+
+        Select Case e.KeyCode
+            Case Keys.Enter
+                Call enterclick()
+            Case Keys.Escape
+                End
+        End Select
+    End Sub
+
+    Private Sub enterclick()
+        FrmMAin.Show()
+        Me.Hide()
+    End Sub
+
+End Class
